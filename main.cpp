3396d6aa4fd59fc9aa5f31c5a8c27fbf1d150d71
GerrickJones <135552049+GerrickJones@users.noreply.github.com>
2024-11-29 15:05:08 -0500

    Function to Display all registers

--- main.cpp
+++ main.cpp
@@ -4,7 +4,41 @@
 
 using namespace std;
 
+const int SIZE = 16;
+
+void PrintRegMem(string *registers, string *memory); // Print registers and memory location data
+
 int main()
 {
-    cout << "yeet";
+   string registers[SIZE] = {"0000", "0001", "0002", "0003", 
+                             "0004", "0005", "0006", "0007", 
+                             "0008", "0009", "000A", "000B",
+                             "000C", "000D", "000E", "000F"}; 
+
+    string memory[SIZE] = {"0000", "0001", "0002", "0003", 
+                           "0004", "0005", "0006", "0007", 
+                           "0008", "0009", "000A", "000B",
+                           "000C", "000D", "000E", "000F"};
+
+    PrintRegMem(registers, memory);
+
+}
+
+
+void PrintRegMem(string *registers, string *memory)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (i < 10)
+        {
+            cout << "\n" << "Reg[$" << i << "]   " << registers[i] << "  "
+         << "Mem[" << i << "]   " << memory[i];
+        }
+        else
+        {
+             cout << "\n" << "Reg[$" << i << "]  " << registers[i] << "  "
+         << "Mem[" << i << "]  " << memory[i];
+        }
+    }
+    
 }
